bf7614cd248dee817c5d3993b51ae2400e2c9fc7
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2022-03-30 21:19:33 -0400

    Update discography.js

--- scripts/discography.js
+++ scripts/discography.js
@@ -2,12 +2,14 @@ gsap.registerPlugin(ScrollTrigger);
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-
-gsap.to('section', {
-    scrollTrigger: 'section',
+gsap.timeline({
+    duration: 2,
+    scrollTrigger: {
     markers: true,
     y: '100%',
     opacity: '100%',
     toggleActions: 'restart complete reverse reset',
-    duration: 2,
-});
+    trigger: 'section'
+    },
+})
+.to('.videoSection', { y: '100%' })
