321d654ef5af8cb18340bedd6071be9058546f40
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2022-03-30 20:48:30 -0400

    Update home.js

--- scripts/home.js
+++ scripts/home.js
@@ -10,6 +10,19 @@ const swiper = new Swiper('.swiper', {
 
 
 
+gsap.registerPlugin(ScrollTrigger);
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+gsap.to('section', {
+    scrollTrigger: 'section',
+    markers: true,
+    y: '100%',
+    opacity: '100%',
+    toggleActions: 'restart complete reverse reset',
+    duration: 2,
+});
 
 
 
