8f9b4bfc252ef207b1d38b6f195d6f62dff33109
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2022-04-13 21:43:06 -0400

    Update discography.js

--- scripts/discography.js
+++ scripts/discography.js
@@ -1,8 +1,6 @@
 gsap.registerPlugin(ScrollTrigger);
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-gsap.timeline({
+/*gsap.timeline({
     duration: 2,
     scrollTrigger: {
     markers: true,
@@ -12,4 +10,35 @@ gsap.timeline({
     trigger: 'section'
     },
 })
-.to('.videoSection', { y: '100%' })
+.to('.videoSection', { y: '100%' })*/
+const body = document.querySelector('body');
+let isScrolling;
+
+window.addEventListener('scroll', function() {
+	window.clearTimeout( isScrolling );
+  body.classList.add("is-scrolling");
+
+	isScrolling = setTimeout(function() {
+    body.classList.remove("is-scrolling");
+	}, 250);
+});
+
+let anim = gsap.timeline({
+  scrollTrigger: {
+    trigger: '#divAnim',
+    
+    onUpdate: (e) => { 
+      
+      if(e.progress){
+         if(e.direction==-1){
+          body.classList.add("direction-up");
+          body.classList.remove("direction-down");
+        }else{
+          body.classList.add("direction-down");
+          body.classList.remove("direction-up");
+        }
+      }
+    }
+  
+  }
+});
