3de352dfedd1225cf10ba2051f0b8198b7749f0d
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2022-04-25 13:54:40 -0400

    fix2

--- scripts/home.js
+++ scripts/home.js
@@ -78,7 +78,7 @@ fetch("https://api.lyrics.ovh/v1/") // L'adresse URL des données à aller cherc
 
 
 //TP 08 ETAPE 5.1
-const isform = document.querySelectorAll('form');
+const isform = document.querySelector('.parolesform');
 
 isform.addEventListener("submit", function(e){
   e.preventDefault();
