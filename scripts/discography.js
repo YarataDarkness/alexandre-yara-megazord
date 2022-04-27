55dd80719fb7c177cc6fa54afb00706764459b30
AlexDan26 <90867270+AlexDan26@users.noreply.github.com>
2022-04-27 15:17:20 -0400

    Update discography.js

--- scripts/discography.js
+++ scripts/discography.js
@@ -42,3 +42,49 @@ let anim = gsap.timeline({
   
   }
 });
+
+const istobefetched = document.querySelector('.tobefectched');
+
+//TP 08 ETAPE 4 et 5.3
+//ETAPE 9: LIVE AND LEARN, open your heart
+fetch("https://api.lyrics.ovh/v1/crush40/") // L'adresse URL des données à aller chercher
+  .then(istobefetched => { // Attendre que les données soient reçues
+    // Convertir les données au format désiré
+  })
+  .then(result => (result),{ // Attendre que les données soient converties
+    // Faire du résultat ce que bon vous semble
+  });
+
+//TP 08 ETAPE 5.1
+const isform = document.querySelector('.parolesform');
+
+isform.addEventListener("submit", function(e){
+  e.preventDefault();
+});
+
+//TP 08 ETAPE 6
+const newLineToBr = function(str) {
+  result;
+  return str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+}
+
+const isdivparoles = document.querySelector('.divforparoles');
+
+//TP 08 ETAPE 7
+const promesse = new Promise((resolve, reject) => {
+  const respectPromesse = true;
+
+  if (respectPromesse === true) {
+    resolve("Promesse respectée");
+  } else {
+    reject("Promesse brisée");
+  }
+});
+
+promesse
+  .then(value => console.log(value))
+  //TP 08 ETAPE 8
+  .catch(isdivparoles => isdivparoles.innerHTML("Désolé, les paroles n'ont pu être trouvées. En voici la raison:"))
+  //.catch(error => console.log(error))
+  .finally(() => console.log("Promesse complétée"));
+
